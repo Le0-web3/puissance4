a7ca46b3c711ab831792eb1ed9918973308c514f
luckyclutcher <luckyclutcher@protonmail.com>
2022-07-11 10:45:08 -0400

    check for a winner

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ const [pattern, setPattern] = useState([
   [0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0],
 ]);
+const [gameState, setGameState] = useState("running");
+const [winnerPlayer, setWinnerPlayer] = useState(0);
 
 //   -----   FUNCTIONS  -----
 
@@ -30,7 +32,6 @@ const getLowestCase = (row, column) => {
   for(let i=row; i<6-row; i++) {
     if(newArr[i][column] === 0) {
       myRow = i;
-      console.log(myRow);
     }
   }
   return myRow;
@@ -41,12 +42,13 @@ const handleClick = (i, s) => {
   const column = s;
   let newArr = [...pattern];
   let lowestRow = row;
-  if(newArr[row][column] === 0) { // if full below
+  if(newArr[row][column] === 0 && gameState === "running") { // if full below
     lowestRow = getLowestCase(row,column);
-    console.log("lowestRow : ",lowestRow)
     newArr[lowestRow][column] = player;
     setPattern(newArr);
     togglePlayer();
+    checkIfWin();
+    console.log(checkIfWin())
   }
 }
 
@@ -60,12 +62,94 @@ const onReset = () => {
     [0, 0, 0, 0, 0, 0, 0],
   ]);
   setPlayer(1);
+  setGameState("running");
+  setWinnerPlayer(0);
+}
+
+const checkIfWin = () => {
+  const myPattern = [...pattern];
+  var win = false;
+  var winner = 0;
+  const regex1 = /1,1,1,1/;
+  const regex2 = /2,2,2,2/;
+
+  // rows
+  for(let i = 0; i < 6; i++) {
+    if(regex1.test(myPattern[i])) {
+      win = true;
+      winner = 1;
+    }
+    if(regex2.test(myPattern[i])) {
+      win = true;
+      winner = 2;
+    }    
+  } // rows
+
+  // columns
+  for(let j = 0; j < 7; j++) {
+    var myColumn = [];
+    for(let i = 0; i < 6; i++) {
+      myColumn.push(myPattern[i][j]);
+    }
+    if(regex1.test(myColumn)) {
+      win = true;
+      winner = 1;
+    }
+    if(regex2.test(myColumn)) {
+      win = true;
+      winner = 2;
+    }    
+  } // columns
+
+  // for / diagonals
+for(let k = 3; k < 9; k++) {
+  var myDiagonal = [];
+    for(let i = 0; i < myPattern.length; i++){
+      let j =  (k - i);
+      if(j>=0 && j < 7) {
+        myDiagonal.push(myPattern[i][j]);
+      } // if
+   }; // i
+
+    if(regex1.test(myDiagonal)) {
+      win = true;
+      winner = 1;
+    }
+    if(regex2.test(myDiagonal)) {
+      win = true;
+      winner = 2;
+    }   
+} // k
+
+// for \ diagonals
+for(let k = -2; k < 4; k++) {
+  var myDiagonal = [];
+    for(let i = 0; i < myPattern.length; i++){
+      let j =  (k + i);
+      if(j>=0 && j < 7) {
+        myDiagonal.push(myPattern[i][j]);
+      } // if
+   }; // i
+    if(regex1.test(myDiagonal)) {
+      win = true;
+      winner = 1;
+    }
+    if(regex2.test(myDiagonal)) {
+      win = true;
+      winner = 2;
+    }   
+} // k
+
+  if(win) {
+    setGameState("won");
+    setWinnerPlayer(winner);
+  }
 }
 
   return (
     <div className="App">
-      <h1>Connect 4</h1>
-      <div id="player" style={{color: "var(--color"+player+")",}}>Player {player} turn</div>
+      <h1> {gameState === "running" ? "connect4" : "player " + winnerPlayer + " won !"} </h1>
+      <div id="player" style={{color: "var(--color"+player+")", visibility: gameState === "won" ? "hidden" : "visible",}}>Player {player} turn</div>
       <div id="resetbutton" onClick={onReset}>Reset</div>
       <div id="gameboard">
       <>
